724ea104170923c91ae78bb1116129ea228599cd
Mattias Duarte <duartemattias4@gmail.com>
2026-01-31 09:10:19 -0400

    agregando dockerfiles

---
+++ dockerfile
@@ -0,0 +1,43 @@
+# ========== DEPENDENCIES ==========
+FROM node:20.19-alpine3.20 as deps
+
+WORKDIR /usr/src/app
+
+COPY package.json ./
+COPY package-lock.json ./
+
+RUN npm install
+
+
+# ========== BUILDER ==========
+FROM node:20.19-alpine3.20 as build
+
+WORKDIR /usr/src/app
+
+COPY --from=deps /usr/src/app/node_modules ./node_modules
+COPY --from=deps /usr/src/app/package*.json ./
+
+COPY . .
+
+RUN npx prisma generate
+
+RUN npm run build
+
+RUN npm ci -f --only=production && npm cache clean --force
+
+# ========== PROD IMAGE ==========
+FROM node:20.19-alpine3.20 as prod
+
+WORKDIR /usr/src/app
+
+COPY --from=build /usr/src/app/node_modules ./node_modules
+COPY --from=build /usr/src/app/prisma ./prisma
+COPY --from=build /usr/src/app/package*.json ./
+COPY --from=build /usr/src/app/src/generated ./src/generated
+COPY --from=build /usr/src/app/dist ./dist
+
+ENV NODE_ENV=production
+
+USER node
+
+CMD ["npm", "run", "start:prod"]
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,28 +1,18 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 import { envs } from 'src/config/envs';
+import { PrismaClient } from 'src/generated/prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient {
 
     private readonly logger = new Logger('Products Database')
 
     constructor() {
-        try {
-            const pool = new Pool({ connectionString: envs.databaseUrl });
-            const adapter = new PrismaPg(pool);
-            super({ adapter });
-            this.logger.log('Connected to Products Database');
-
-        } catch (error) {
-            super();
-            this.logger.error('Failed to connect to Products Database', error);
-        }
-    }
-
-    async onModuleInit() {
-        await this.$connect();
+        const pool = new Pool({ connectionString: envs.databaseUrl });
+        const adapter = new PrismaPg(pool);
+        super({ adapter });
+        this.logger.log('Connected to Products Database');
     }
 }
